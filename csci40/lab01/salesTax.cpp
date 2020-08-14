87c625f0dbb9cd9727d2e5c965a81a57d6d92921
Lawton Nichols <lawtonnichols@gmail.com>
2020-08-13 17:17:14 -0700

    .

--- csci40/lab01/salesTax.cpp
+++ csci40/lab01/salesTax.cpp
@@ -11,15 +11,16 @@ int main() {
   cout << "Enter the price: ";
   cin >> price;
 
-  // we're living in an alternate universe where there's no pennies or nickels,
+  // we're living in an alternate universe where there are no pennies or nickels,
   // so everything is rounded to the nearest 10 cents
-  // 7.75% tax, *rounded to the nearest dime*
-  // come to office hours and I'll give you a hint
+
+  // Your program will calculate 7.75% tax, *rounded to the nearest dime*
+  // Come to office hours and I'll give you a hint!
 
   // FIXME: print the following on separate lines
   //        (all #s rounded to the nearest 2 decimal places):
   //          1. the price
-  //          2. the sales tax calculated for the price
+  //          2. the (rounded) sales tax calculated for the price, with a '+' at the front
   //          3. a line with 10 '=' characters
   //          4. the total price
   //
