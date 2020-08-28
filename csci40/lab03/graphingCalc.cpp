a44a90af4c3f7205d15283fd758f6a66ff47b3df
Lawton Nichols <lawtonnichols@gmail.com>
2020-08-27 20:59:48 -0700

    .

--- csci40/lab03/graphingCalc.cpp
+++ csci40/lab03/graphingCalc.cpp
@@ -13,15 +13,18 @@ int main() {
     for (int x = -38; x <= 39; x++) {
       // the current coordinate is (x, y)
 
-      // FIXME: check if the current coordinate is in the graph that the user chose
-      // (make sure to round() the y coordinate!)
+      // FIXME: check if the current coordinate is in the graph that the user chose.
+      // Make sure to round() the y coordinate for a given x coordinate!
+      // For example, if the user chose choice 0 (y = 0.15x^2 - 3), then you should check
+      // whether rounding 0.15x^2 - 3 equals y--if it does, then this point should be plotted
+      // on our graph.
 
-      // FIXME: if it is, print a '*' character at this coordinate and continue 
+      // FIXME: if (x, y) is on the graph, print a '*' character at this coordinate and continue 
       // to the next loop iteration
 
       // FIXME: if we don't hit a point on the graph, print either the axis line or a space
-      //   - if y = 0, print a vertical axis line here
-      //   - if x = 0, print a horizontal axis line here
+      //   - if y = 0, print a vertical axis line here ('|')
+      //   - if x = 0, print a horizontal axis line here ('-')
       //   - if x = 0 and y = 0, print the axes crossing ('+')
       //   - if none of the above are true, print a single blank space (' ')
     }
