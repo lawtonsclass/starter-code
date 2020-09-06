e89e43c0ca23d622d8f7384c203eef7c65050c59
Lawton Nichols <lawtonnichols@gmail.com>
2020-09-05 17:33:17 -0700

    .

--- csci40/lab03/graphingCalc.cpp
+++ csci40/lab03/graphingCalc.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
   int choice;
 
-  cout << "Enter 0, 1, or 2 (0 -> 0.15*x^2 - 3, 1 -> 0.15*x^3 + 2, 2 -> 5*sin(0.25*x)): ";
+  cout << "Enter 0, 1, or 2 (0 -> (0.15*x)^2 - 3, 1 -> (0.15*x)^3 + 2, 2 -> 5*sin(0.25*x)): ";
   cin >> choice;
   cout << endl;
 
@@ -15,8 +15,8 @@ int main() {
 
       // FIXME: check if the current coordinate is in the graph that the user
       // chose. Make sure to round() the y coordinate for a given x coordinate!
-      // For example, if the user chose choice 0 (y = 0.15x^2 - 3), then you
-      // should check whether rounding 0.15x^2 - 3 equals y--if it does, then
+      // For example, if the user chose choice 0 (y = (0.15x)^2 - 3), then you
+      // should check whether rounding (0.15x)^2 - 3 equals y--if it does, then
       // this point should be plotted on our graph.
 
       // FIXME: if (x, y) is on the graph, print a '*' character at this
