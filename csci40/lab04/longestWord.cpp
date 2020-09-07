89135cdff24ca73809b4c0506a66041b3da1b49a
Lawton Nichols <lawtonnichols@gmail.com>
2020-09-07 00:23:34 -0700

    .

--- csci40/lab04/longestWord.cpp
+++ csci40/lab04/longestWord.cpp
@@ -3,7 +3,6 @@ using namespace std;
 
 int main() {
   // FIXME: read words from cin, and keep track of which one is the biggest
-  // FIXME: output "The longest word is: ", and then print the longest word (on that same line)
 
   string currentWord;
   // the following while loop keeps getting a word from cin and putting it into
@@ -12,6 +11,9 @@ int main() {
   while (cin >> currentWord) {
     // FIXME
   }
+
+  // FIXME: output "The longest word is: ", and then print the longest word (on that same line)
+
 
   return 0;
 }
