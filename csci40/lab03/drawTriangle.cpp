3fb19dd8cf03f854ec349067457b9c0f491f8c2c
Lawton Nichols <lawtonnichols@gmail.com>
2021-01-28 20:43:46 -0800

    .

--- csci40/lab03/drawTriangle.cpp
+++ csci40/lab03/drawTriangle.cpp
@@ -10,11 +10,7 @@ int main() {
   if (size < 0) {
     cout << "Invalid size." << endl;
     // FIXME: stop the program if the size was invalid.
-    // You should exit with a return code of 1.
-
-    // The autograder output will look a little different in this case because
-    // it sees the nonzero return code, but there's nothing extra you need to
-    // print other than the "Invalid size." above.
+    // You should exit the program with exit or return.
   }
 
   // FIXME: print a (right-leaning) right triangle of the requested size
